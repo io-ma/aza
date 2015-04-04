9358580bf7d23bf514e45bbbcf0140c33b511141
io-ma <io.manoliu@gmail.com>
2015-04-04 13:18:04 +0300

    index.html
    recreated index.html

--- includes/header.php
+++ includes/header.php
@@ -1,16 +1,16 @@
 <?php
 echo '"<div class="header">
 					<div class="logo">
-					<img src="images/logo4-2.png" width="187" height="37" alt="azabrasov-constructii-logo"> 
+					<img src="/../images/logo4-2.png" width="187" height="37" alt="azabrasov-constructii-logo"> 
 					</div><!--logo neds here-->
 					<div class="menu">
 						<ul>
-							<li><a href="index.html">Home</a></li>
-                    			<li><a href="desprenoi.html">Despre noi</a></li>
-                            			<li><a href="solutii.html">Solutii</a></li>
-                            <li><a href="photo.html">Galerie foto</a></li>
-                             <li><a href="noutati.html">Cariere</a></li>
-                            <li><a href="contact.html">Contact</a></li>
+							<li><a href="../index.php">Home</a></li>
+                    			<li><a href="../desprenoi.html">Despre noi</a></li>
+                            			<li><a href="../solutii.html">Solutii</a></li>
+                            <li><a href="../photo.html">Galerie foto</a></li>
+                             <li><a href="../cariere.html">Cariere</a></li>
+                            <li><a href="../contact.html">Contact</a></li>
                       	</ul><!--mainmenu ends here-->
 					</div> <!-- menu ends here -->
 				</div> <!--header ends here -->"';
