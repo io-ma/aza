9696400b0ccfdb2dcf72513e1dbe748256c5a40f
io-ma <io.manoliu@gmail.com>
2015-04-04 11:13:27 +0300

    Changed menu, php includes
    -Deleted the submenus. Will add them later on the "desprenoi" page.
    -Changed the "noutati" link to "cariere".
    -Changed "servicii" to "solutii"
    -added php includes header and footer
    -created a folder for includes
    -deleted index.html

---
+++ includes/footer.php
@@ -0,0 +1,5 @@
+<?php
+echo'<div class="footer">
+              	<div class="copyright-wrapper"><p class="copyright"> All rights reserved © S.C. Aza Brasov 2015</div><!--copyright ends here--></div><!--copyright-wrapper ends here-->
+                </div><!--footer ends here-->';
+?>
